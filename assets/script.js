75fb1902c5245ff734e25756e65536a9db014633
Justin 석원 Choi <justinschoi93@gmail.com>
2023-12-03 07:30:25 -0800

    skeleton complete

---
+++ assets/script.js
@@ -0,0 +1,92 @@
+const questions = [
+  {
+    question: 'Which album is this? Display img here',
+    answers: {
+        a: 'Un Verano Sin Ti',
+        b: 'Link to img',
+        c: 'Link to img',
+        d: 'Link to img',
+    },
+    correctAnswer: 'A'
+  },
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+  {
+    question: '',
+    answers: {
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+    },
+    correctAnswer: ''
+  }  
+];
