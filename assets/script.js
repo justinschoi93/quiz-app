2abe09517463462da330ec645478b9c61e99b366
Justin 석원 Choi <justinschoi93@gmail.com>
2023-12-11 17:02:30 -0800

    ran into CORS error. uploading to pages.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,128 @@
-let qIndex = 0;
-const questions = 
+const questions = [
+    {
+        question: "What is Bad Bunny's real name?",
+        answers: {
+            a: 'Ricardo Martin',
+            b: 'Benito Antonio Martínez Ocasio',
+            c: 'Luis Fonsi',
+            d: 'Rafael Pérez'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which of these songs is a hit single by Bad Bunny?",
+        answers: {
+            a: 'Despacito',
+            b: 'Dákiti',
+            c: 'Vivir Mi Vida',
+            d: 'El Perdón'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "In which year did Bad Bunny release his debut album 'X 100pre'?",
+        answers: {
+            a: '2018',
+            b: '2017',
+            c: '2019',
+            d: '2020'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Bad Bunny is known for being a pioneer in which music genre?",
+        answers: {
+            a: 'Reggaeton',
+            b: 'Latin Trap',
+            c: 'Salsa',
+            d: 'Bachata'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which popular song did Bad Bunny collaborate on with Cardi B and J Balvin?",
+        answers: {
+            a: 'I Like It',
+            b: 'MIA',
+            c: 'Callaíta',
+            d: 'Tusa'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is the name of Bad Bunny's third studio album, released in 2020?",
+        answers: {
+            a: 'YHLQMDLG',
+            b: 'El Último Tour Del Mundo',
+            c: 'X 100pre',
+            d: 'Oasis'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Bad Bunny made his acting debut in which season of 'Narcos: Mexico'?",
+        answers: {
+            a: 'Season 2',
+            b: 'Season 3',
+            c: 'Season 4',
+            d: 'Season 1'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which 2022 sports event did Bad Bunny perform at?",
+        answers: {
+            a: 'Super Bowl Halftime Show',
+            b: 'NBA All-Star Game',
+            c: 'FIFA World Cup Opening Ceremony',
+            d: 'Olympic Games Opening Ceremony'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "What is the title of Bad Bunny's collaboration with Drake?",
+        answers: {
+            a: 'Te Boté',
+            b: 'MIA',
+            c: 'La Canción',
+            d: 'Vete'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "In addition to his music career, Bad Bunny has also been involved in which sport?",
+        answers: {
+            a: 'Basketball',
+            b: 'Soccer',
+            c: 'Wrestling',
+            d: 'Boxing'
+        },
+        correctAnswer: 'c'
+    }
+];
 
-function checkAnswer ( qObj, theChosenOne ) {
+document.addEventListener("DOMContentLoaded", () => {
+
+    const question = document.getElementById("question-window");
+    const answers = document.getElementById("answers-window");
+    const answerA = document.getElementById("answer-a");
+    const answerB = document.getElementById("answer-b");
+    const answerC = document.getElementById("answer-c");
+    const answerD = document.getElementById("answer-d");
     
-}
+    let qIndex = 0;
+
+    question.innerHTML = questions[qIndex].question;
+    answerA.innerHTML = questions[qIndex].answers.a;
+    answerB.innerHTML = questions[qIndex].answers.b;
+    answerC.innerHTML = questions[qIndex].answers.c;
+    answerD.innerHTML = questions[qIndex].answers.d;
+
+    answers.addEventListener("click", (e)=>{
+        console.log(e);
+    })
+})
+
+// function checkAnswer ( qObj, theChosenOne ) {
+    
+// }
